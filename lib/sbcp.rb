5465ef591e19b2ea052b5f6cbe64fc1d6472f2f1
Kazyyk <kazyyk@host.kazyyk.com>
2017-01-30 15:11:19 +0100

    Prep for Sinatra rewrite

--- lib/sbcp.rb
+++ lib/sbcp.rb
@@ -1,5 +1,5 @@
 # SBCP - Starbound Server Management Solution for Linux Servers
-# Copyright (C) 2016 Kazyyk
+# Copyright (C) 2016-2017 Kobold Software (Kazyyk)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,300 +14,45 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-require 'highline/import'
-require 'celluloid/current'
-require 'time_diff'
-require 'tempfile'
+require 'sinatra/base'
+require 'sinatra/contrib'
+require 'sinatra/flash'
+require 'securerandom'
+require 'fileutils'
+require 'logger'
 require 'json'
-require 'hirb'
-require 'pp'
-
-require_relative 'sbcp/backup'
-require_relative 'sbcp/config'
 require_relative 'sbcp/daemon'
-require_relative 'sbcp/database'
-require_relative 'sbcp/rcon'
-require_relative 'sbcp/setup'
-
-Hirb::View.enable
 
 module SBCP
-	class SBCP
+	class WebServer < Sinatra::Base
+		register Sinatra::Contrib
+		register Sinatra::Flash
+		configure do
+			set :environment, :development
+			set :server, 'thin'
+			set :threaded, true
+			set :bind, '0.0.0.0'
+			use Rack::Session::Cookie, 
+				:key => 'rack.session',
+				:path => '/',
+				:expire_after => 3600
+		end
+		run! if app_file == $0
+
 		def initialize
-			@commands = ['backup', 'clear', 'config', 'detach', 'exit', 'get', 'kill', 'promote','quit', 'reboot', 'restart', 'say', 'start', 'stop', 'help']
-			$database = Database.new()
-			@commands_scheme = [
-				"<%= color('backup', :command) %>",
-				"<%= color('clear', :command) %>",
-				"<%= color('config', :command) %>",
-				"<%= color('detach', :command) %>",
-				"<%= color('exit', :command) %>",
-				"<%= color('get', :command) %>",
-				"<%= color('kill', :command) %>",
-				"<%= color('promote', :command) %>",
-				"<%= color('quit', :command) %>",
-				"<%= color('reboot', :command) %>",
-				"<%= color('restart', :command) %>",
-				"<%= color('say', :command) %>",
-				"<%= color('start', :command) %>",
-				"<%= color('stop', :command) %>",
-				"<%= color('help', :command) %>"
-			]
-			scheme = HighLine::ColorScheme.new do |cs|
-				cs[:headline] =		[ :bold, :yellow, :on_black ]
-				cs[:subheader] =	[ :cyan, :on_black ]
-				cs[:command] =		[ :green, :on_black ]
-				cs[:warning] = 		[ :red, :on_black ]
-				cs[:failure] =		[ :bold, :red, :on_black ]
-				cs[:success] =		[ :bold, :green, :on_black]
-				cs[:info] =			[ :bold, :blue, :on_black ]
-				cs[:help] =			[ :magenta, :on_black ]
-			end
-			HighLine.color_scheme = scheme
-			if File.exist?(File.expand_path('../../config.json', __FILE__))
-				$settings = JSON.parse(File.read(File.expand_path('../../config.json', __FILE__)))
-			else
-				Setup.new.run # lib/setup.rb
-				$settings = JSON.parse(File.read(File.expand_path('../../config.json', __FILE__)))
-			end
-		rescue Psych::SyntaxError
-			abort("SBCP was unable to load the configuration file. It may be corrupt.")
-		end
-
-		def intro
-			system('clear')
-			say("<%= color(' .d8888b.  888888b.    .d8888b.  8888888b.', :headline) %>")
-			say("<%= color('d88P  Y88b 888  \"88b  d88P  Y88b 888   Y88b', :headline) %>")
-			say("<%= color('Y88b.      888  .88P  888    888 888    888', :headline) %>")
-			say("<%= color(' \"Y888b.   8888888K.  888        888   d88P', :headline) %>")
-			say("<%= color('    \"Y88b. 888  \"Y88b 888        8888888P\"', :headline) %>")
-			say("<%= color('      \"888 888    888 888    888 888', :headline) %>")
-			say("<%= color('Y88b  d88P 888   d88P Y88b  d88P 888', :headline) %>")
-			say("<%= color(' \"Y8888P\"  8888888P\"   \"Y8888P\"  888', :headline) %>")
-			say("<%= color('       (    (        *       ) (', :subheader) %>")
-			say("<%= color('   (   )\\ ) )\\ )   (  `   ( /( )\\ )', :subheader) %>")
-			say("<%= color('   )\\ (()/((()/(   )\\))(  )\\()|()/(  (', :subheader) %>")
-			say("<%= color(' (((_) /(_))/(_)) ((_)()\\((_)\\ /(_)) )', :subheader) %>")
-			say("<%= color(' )\\___(_)) (_))   (_()((_) ((_|_))_ ((_)', :subheader) %>")
-			say("<%= color('((/ __| |  |_ _|  |  \\/  |/ _ \\|   \\| __|', :subheader) %>")
-			say("<%= color(' | (__| |__ | |   | |\\/| | (_) | |) | _|', :subheader) %>")
-			say("<%= color('  \\___|____|___|  |_|  |_|\\___/|___/|___|', :subheader) %>")
-			say("\n")
-		end
-
-		def repl
-			intro
-			loop do
-				input = ask('> ')
-				case input
-				when /^(backup|backup\s?(\S+))$/
-					backup($2)
-				when 'clear'
-					system('clear')
-				when 'config'
-					Config.new.run # lib/config.rb
-					intro
-				when 'detach'
-					system('screen -d')
-				when 'exit', 'quit'
-					say("<%= color('!!! This action could result in data loss !!!', :warning) %>")
-					say("<%= color('!!! Starbound shall be stopped if running !!!', :warning) %>")
-					if agree("Are you sure? ", true)
-						stop('SIGKILL') unless `pidof starbound_server`.empty? # prevent abort request warning if SB not running
-						exit
-					end
-				when /^(get|get\s?(\S+))$/
-					get($2)
-				when 'kill'
-					say("<%= color('!!! This action could result in data loss !!!', :warning) %>")
-					if agree("Are you sure? ", true)
-						stop('SIGKILL')
-					end
-				when /^(promote|promote\s?(.+))$/
-					promote($2)
-				when 'reboot'
-					say("<%= color('!!! This action could result in data loss !!!', :warning) %>")
-					if agree("Are you sure? ", true)
-						restart('SIGKILL')
-					end
-				when 'restart'
-					if agree("Are you sure? ", true)
-						restart
-					end
-				when /^(say|say\s?(.+))$/
-					sb_say($2)
-				when 'start'
-					if not $settings['system']['starbound'].nil?
-						if `pidof starbound_server`.empty?
-							if $daemon.nil?
-								start
-							else
-								say("<%= color('Duplicate prevented.', :warning) %> The daemon is still processing. Please wait and try again.")
-							end
-						else
-							say("<%= color('Duplicate prevented.', :warning) %> The server is already running.")
-						end
-					else
-						say("<%= color('Error', :warning) %> Starbound directory path is invalid. Check the configuration setup. (type 'config')")
-					end
-				when 'stop'
-					if agree("Are you sure? ", true)
-						stop
-					end
-				when /^(help|help\s?(\S+))$/
-					command = $2
-					if not command.nil?
-						if @commands.include? command.strip
-							help(command)
-						else
-							say("The command \"#{command.strip}\" does not exist.")
-						end
-					else
-						help
-					end
-				else
-					say('Invalid command. Try help for a list of possible commands.')
-				end
-			end
-		ensure
-			$daemon.terminate unless $daemon.nil?
-		end
-
-		def backup(type=nil)
-			if not type.nil?
-				case type
-				when 'starbound', 'sbcp', 'full'
-					Backup.create_backup(type)
-				else
-					say("Backup type \"#{type}\" is not valid.")
-				end
-			else
-				say('Please specify a backup type.')
-			end
-		end
-
-		def get(data=nil)
-			if not data.nil?
-				case data
-				when 'info'
-					unless Starbound::SESSION.nil? || Starbound::SESSION.empty?
-						Starbound::SESSION[:info][:uptime] = Time.diff(Starbound::SESSION[:info][:started], Time.now, '%H %N %S')[:diff]
-						unless Starbound::SESSION[:info][:restart_in] == 'Never'
-							Starbound::SESSION[:info][:restart_in] = "#{((($settings['restarts']['frequency']*60*60) - (Time.now - Starbound::SESSION[:info][:started]))/60).to_i} minutes" # Inaccurate for first start, fairly acurrate thereafter
-						end
-						Starbound::SESSION[:info].each_pair do |key, value|
-							say("<%= color('#{key.to_s.capitalize}:', :info) %> #{value}")
-						end
-					else
-						say("<%= color('Error!', :failure) %> Session data is missing or empty.")
-					end
-				when 'players'
-					unless Starbound::SESSION.nil? || Starbound::SESSION.empty?
-						Hirb::View.capture_and_render { pp(Starbound::SESSION[:players]) }
-					else
-						say("<%= color('Error!', :failure) %> Session data is missing or empty.")
-					end
-				else
-					say("Data type \"#{data}\" is not valid.")
-				end
-			else
-				say('Please specify a data type.')
-			end
-		end
-
-		def restart(signal='SIGTERM')
-			say('Sending restart request...')
-			kill(signal)
+			# TODO
 		end
 
 		def start
-			say('Starting the Starbound server...')
-			supervisor = Daemon.supervise
-			$daemon = supervisor.actors.first
-			$daemon.async.start
-			say("<%= color('Operation complete.', :success) %>")
+			# TODO
 		end
 
-		def stop(signal='SIGTERM')
-			say('Sending stop request...')
-			file = Tempfile.new('sb-shutdown')
-			kill(signal)
-		ensure
-			unless file.nil?
-				file.close
-				file.unlink
-			end
+		def restart
+			# TODO
 		end
 
-		def promote(args)
-			username = args.split(' ', 2)[0]
-			level = args.split(' ', 2)[1]
-			level_code = nil
-			case level
-			when 'user'
-				level_code = Models::Account::USER
-			when 'mod'
-				level_code = Models::Account::MOD
-			when 'techmod'
-				level_code = Models::Account::TECHMOD
-			when 'admin'
-				level_code = Models::Account::ADMIN
-			end
-
-			if not level_code.nil?
-				if $database.promote_account(username, level_code)
-					say("<%= color('User promoted.', :success) %>")
-				else
-					say("<%= color('Promotion failed.', :warning) %>")
-				end
-			else
-				say("<%= color('Invalid Level. Try: user, mod, techmod, admin.', :warning) %>")
-			end			
-		end
-
-		def sb_say(string)
-			unless string.nil?
-				if not $rcon.nil?
-					$rcon.execute("say #{string}")
-					say("<%= color('Message sent to server.', :success) %>")
-				else
-					say("<%= color('RCON is not running.', :warning) %>")
-				end
-			else
-				say("Please type something to say.")
-			end
-		end
-
-		def kill(signal='SIGTERM')
-			pid = `pidof starbound_server`
-			unless pid.empty?
-				system("kill -s #{signal} #{pid.to_i}")
-				t = Time.now
-				d = 0
-				until `pidof starbound_server`.empty? || d >= 5
-					sleep(1)
-					d = Time.now - t
-				end
-				if not `pidof starbound_server`.empty?
-					return say("<%= color('Failure!', :failure) %> The server is still running.")
-				end
-				return say("<%= color('Operation complete.', :success) %>")
-			end
-			say("<%= color('Aborting request. See details below.', :failure) %>")
-			return say('Unable to locate the starbound_server process. Is it running?')
-		end
-
-		def help(command=nil)
-			if not command.nil?
-				case command
-				when 'backup'
-				else
-					say("Could not find help for \"#{command}\" command.")
-				end
-			else
-				say("<%= color('Command list. Type help [command] to learn more.', :help) %>")
-				say("#{@commands_scheme.join(", ")}")
-			end
+		def stop
+			# TODO
 		end
 	end
 end
--- sbcp.gemspec
+++ sbcp.gemspec
@@ -7,7 +7,7 @@ Gem::Specification.new do |spec|
   spec.name          = 'sbcp'
   spec.version       = SBCP::VERSION
   spec.authors       = ['Kazyyk']
-  spec.email         = ['kazyyk@me.com']
+  spec.email         = ['support@koboldsoftware.net']
 
   spec.summary       = %q{SBCP is a Starbound server management solution for Linux.}
   spec.homepage      = 'https://github.com/Kazyyk/SBCP'
@@ -17,24 +17,21 @@ Gem::Specification.new do |spec|
   spec.bindir        = 'bin'
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.require_paths = ['lib']
-  spec.required_ruby_version = '>= 2.3.0'
+  spec.required_ruby_version = '>= 2.3.3'
 
-  #spec.add_runtime_dependency 'sinatra', '~> 1.4', '>= 1.4.7'
-  #spec.add_runtime_dependency 'sinatra-flash', '~> 0.3.0'
-  #spec.add_runtime_dependency 'sinatra-contrib', '~> 1.4', '>= 1.4.7'
-  #spec.add_runtime_dependency 'thin', '~> 1.6', '>= 1.6.4'
-  #spec.add_runtime_dependency 'sqlite3', '~> 1.3', '>= 1.3.11'
-  #spec.add_runtime_dependency 'data_mapper', '~> 1.2'
-  #spec.add_runtime_dependency 'dm-sqlite-adapter', '~> 1.2'
+  spec.add_runtime_dependency 'sinatra', '~> 1.4', '>= 1.4.7'
+  spec.add_runtime_dependency 'sinatra-flash', '~> 0.3.0'
+  spec.add_runtime_dependency 'sinatra-contrib', '~> 1.4', '>= 1.4.7'
+  spec.add_runtime_dependency 'thin', '~> 1.6', '>= 1.6.4'
+  spec.add_runtime_dependency 'sqlite3', '~> 1.3', '>= 1.3.11'
+  spec.add_runtime_dependency 'data_mapper', '~> 1.2'
+  spec.add_runtime_dependency 'dm-sqlite-adapter', '~> 1.2'
   spec.add_runtime_dependency 'celluloid', '~> 0.17.3'
   spec.add_runtime_dependency 'rufus-scheduler', '~> 3.2'
   spec.add_runtime_dependency 'steam-condenser', '~> 1.3', '>= 1.3.11'
   spec.add_runtime_dependency 'rsync', '~> 1.0', '>= 1.0.9'
   spec.add_runtime_dependency 'rbzip2', '~> 0.2.0' # seven_zip_ruby would not compile, using this instead
-  spec.add_runtime_dependency 'highline', '~> 1.7', '>= 1.7.8'
   spec.add_runtime_dependency 'time_diff', '~> 0.3.0'
-  spec.add_runtime_dependency 'hirb', '~> 0.7.3'
-
 
   spec.add_development_dependency 'bundler', '~> 1.11'
   spec.add_development_dependency 'rake', '~> 10.0'
